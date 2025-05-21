71c8f240d34225debd3746f9d766b2e8e28dec08
Edson <EdsonP4100@gmail.com>
2025-05-20 17:58:58 -0700

    Recipe

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -4,20 +4,25 @@ import {
   getRecipeById,
   getRecipes,
 } from '../services/RecipeService';
-import { Categories, Drink, Drinks } from '../types';
+import { Categories, Drink, Drinks, Recipe } from '../types';
 import { SearchFilter } from '../types/index';
 
 export type RecipeSliceType = {
   categories: Categories;
   drinks: Drinks;
+  selectedRecipe: Recipe;
+  modal: boolean;
   fetchCategories: () => Promise<void>;
   searchRecipes: (searchFilters: SearchFilter) => Promise<void>;
   selectRecipe: (id: Drink['idDrink']) => Promise<void>;
+  closeModal: () => void;
 };
 
 export const createRecipesSlice: StateCreator<RecipeSliceType> = (set) => ({
   categories: { drinks: [] },
   drinks: { drinks: [] },
+  selectedRecipe: {} as Recipe,
+  modal: false,
   fetchCategories: async () => {
     const categories = await getCategories();
     set({
@@ -32,6 +37,15 @@ export const createRecipesSlice: StateCreator<RecipeSliceType> = (set) => ({
   },
   selectRecipe: async (id) => {
     const selectedRecipe = await getRecipeById(id);
-    console.log(selectedRecipe);
+    set({
+      selectedRecipe,
+      modal: true,
+    });
+  },
+  closeModal: () => {
+    set({
+      modal: false,
+      selectedRecipe: {} as Recipe,
+    });
   },
 });
